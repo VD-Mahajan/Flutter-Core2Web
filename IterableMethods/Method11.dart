cac292708fee0c7a592a3efe1ad587c3f2534848
unknown <mahajanvishal410a@gmail.com>
2023-11-05 10:03:59 +0530

    .\IterableMethods\Method11.dart added

---
+++ IterableMethods/Method11.dart
@@ -0,0 +1,8 @@
+void main(){
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
+
+    // THe current elements of this iterable modified by toElement
+    var value = players.map((element)=>element+"India"); 
+    print(value);
+
+}
