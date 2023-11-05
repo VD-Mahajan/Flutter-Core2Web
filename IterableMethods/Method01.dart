d14739f7111921f81f0ea44205f8d93ce8c635e1
unknown <mahajanvishal410a@gmail.com>
2023-11-05 09:05:53 +0530

    .\IterableMethods\Method01.dart modified

--- IterableMethods/Method01.dart
+++ IterableMethods/Method01.dart
@@ -1,5 +1,5 @@
 void main(){
-    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul",];
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
     
     bool value = players.any((player)=>player[0]=="D");                 // any() returns bool value
     print(value);
