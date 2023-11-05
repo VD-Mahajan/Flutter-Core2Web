fcbbff07a7e406c17ec1b67484c62ef14df786bd
unknown <mahajanvishal410a@gmail.com>
2023-11-05 09:53:22 +0530

    .\IterableMethods\Method09.dart added

---
+++ IterableMethods/Method09.dart
@@ -0,0 +1,7 @@
+void main(){
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
+
+    // its return type is void and it invokes action on each element of this iterable in iteration order
+    players.forEach((element)=>print(element));  
+    
+}
