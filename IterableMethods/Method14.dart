bee2cd806046b103e39ff42fde32acbe6a5ee1ef
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-11-05 10:24:14 +0530

    .\IterableMethods\Method14.dart added

---
+++ IterableMethods/Method14.dart
@@ -0,0 +1,7 @@
+void main(){
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
+
+    var value = players.skip(4);    // skips first 4 elements
+    
+    print(value);
+}
