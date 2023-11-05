e7502058f93a7845e41fcc43c45c01b9bf594a1a
unknown <mahajanvishal410a@gmail.com>
2023-11-05 09:05:11 +0530

    .\IterableMethods\Method02.dart added

---
+++ IterableMethods/Method02.dart
@@ -0,0 +1,8 @@
+void main(){
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
+    
+    bool value1 = players.contains("Rohit");                 // contains() returns bool value
+    bool value2 = players.contains("Vishal");
+    print(value1);
+    print(value2);
+}
