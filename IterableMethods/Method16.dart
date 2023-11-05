a4f990de7b83615b8b971ca4b4f41733e14c2949
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-11-05 10:29:31 +0530

    .\IterableMethods\Method16.dart added

---
+++ IterableMethods/Method16.dart
@@ -0,0 +1,7 @@
+void main(){
+    var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
+
+    var value = players.take(2);    // takes first 2 elements
+    
+    print(value);
+}
