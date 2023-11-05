5e2d1171279d816358cf8a8b7c4f3592b06d59f6
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-11-05 10:10:06 +0530

    modification in Method18.dart

--- IterableMethods/Method18.dart
+++ IterableMethods/Method18.dart
@@ -1,10 +1,9 @@
 void main(){
     var players = ["Dhoni","Virat","Rohit","Shubhman","Hardik","Shami","KLRahul"];
 
-    // THe current elements of this iterable modified by toElement
     var value = players.map((element)=>element+"India"); 
 
-    List ans = value.toList();
+    List ans = value.toList();          // converts Iterable<String> to List<String>
     print(value);
     print(ans);
     print(value.runtimeType);
