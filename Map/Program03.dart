9938d02b89ecf66ea53f19dacd57689e0830f528
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-12-19 23:17:28 +0530

    Initial commit

--- Map/Program03.dart
+++ Map/Program03.dart
@@ -2,7 +2,7 @@ import 'dart:collection';
 
 void main() {
   var map = HashMap();
-  var planets = <int, String>{1: "Earth", 2: "Mars"}.entries;
+  var planets = <int, String>{1: "Earth",5:"Jupyter", 2: "Mars",}.entries;
   map.addEntries(planets);
   print(map);
 }
