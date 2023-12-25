f1880c5cc2d70cad3480cd0cbaac624cd3313840
Vishal Mahajan <mahajanvishal410a@gmail.com>
2023-12-25 18:52:27 +0530

    Initial commit

---
+++ Queue/Program04.dart
@@ -0,0 +1,12 @@
+void main() {
+  var players = ["Dhoni", "Rohit", "Virat", "Sachin"];
+  print(players);
+
+  print(players.runtimeType);
+  print(players.first);
+  print(players.last);
+  print(players.length);
+  print(players.hashCode);
+
+  print(players.iterator);
+}
