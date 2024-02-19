b36feb0b9d7e83d0c3ca88a9da9f58afd20fb5aa
Vishal Mahajan <mahajanvishal410a@gmail.com>
2024-02-19 16:09:42 +0530

    Initial commit

---
+++ Quizs/For loop 2/Program09.dart
@@ -0,0 +1,7 @@
+void main() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      print(2*j+i);
+    }
+  }
+}
---
+++ Quizs/Functions/Program02.dart
@@ -0,0 +1,14 @@
+void fun() {
+  int x = 10;
+  print("In fun");
+  print(x);
+  return;
+  print("end gun");
+}
+
+void main() {
+  int x = 100;
+  print("In main");
+  fun();
+  print("end main");
+}
